89f1c14f3231089a67f8968a4716a3ef9b1238b4
nelsonju100 <junior.m123.nm@gmail.com>
2021-07-27 19:30:10 -0300

    texte get props ROCKETSEAT

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-function Home(){
-    return <div>Home3 JUUUU</div>
+
+export default function Home({org}){
+    return (
+    <div>
+        <h1>INICIO</h1>
+        <h1>{org.login}</h1>
+        <h3>{org.description}</h3>
+
+        <p>Site: <a href={org.blog}>{org.blog}</a></p>
+    </div>
+    )
 }
 
-export default Home
+export const getStaticProps = async () => {
+    const response = await fetch('https://api.github.com/orgs/rocketseat');
+    const data = await response.json();
+  
+    return {
+        props: {
+            org: data,
+        }
+    }
+};
+
